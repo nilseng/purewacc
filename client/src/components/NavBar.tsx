fd8f5d00f4ea1a2a9a0d8e3043e0cc03706217ff
Teodor Nilseng Danielsen <teodor.danielsen@gmail.com>
2020-06-18 22:14:22 +0200

    adding auth0 authorization frontend

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
 import { FontAwesomeIcon as FaIcon } from "@fortawesome/react-fontawesome";
-import { faSearchDollar } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSearchDollar,
+  faKey,
+  faBan,
+} from "@fortawesome/free-solid-svg-icons";
+import { useAuth0 } from "../react-auth0-spa";
 
 const NavBar = () => {
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
   return (
     <Navbar bg="dark" expand="md" collapseOnSelect>
       <Navbar.Brand style={{ color: "#f8f9fa" }}>
@@ -15,10 +24,30 @@ const NavBar = () => {
         aria-controls="basic-navbar-nav"
         style={{ outline: "none" }}
       />
-      <Navbar.Collapse
-        id="basic-navbar-nav"
-        className="justify-content-end"
-      ></Navbar.Collapse>
+      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+        <Nav>
+          {!isAuthenticated && (
+            <Nav.Item>
+              <Button
+                variant="link"
+                className="mr-2"
+                onClick={() => loginWithRedirect({})}
+              >
+                <FaIcon icon={faKey} style={{ marginRight: "0.4rem" }}></FaIcon>
+                Log In
+              </Button>
+            </Nav.Item>
+          )}
+          {isAuthenticated && (
+            <>
+              <Nav.Link className="mr-2" onClick={() => logout()}>
+                <FaIcon icon={faBan} style={{ marginRight: "0.4rem" }}></FaIcon>
+                Log out
+              </Nav.Link>
+            </>
+          )}
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
