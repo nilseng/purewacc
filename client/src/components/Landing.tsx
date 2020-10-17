fb4a433348944cf0636bbb0761b1783de6176a70
Teodor Nilseng Danielsen <teodor.nilseng@gmail.com>
2020-10-17 20:57:22 +0200

    showing create project button to unauth users also

--- client/src/components/Landing.tsx
+++ client/src/components/Landing.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import { FontAwesomeIcon as FaIcon } from "@fortawesome/react-fontawesome";
-import { faKey, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
-import { useAuth0 } from "../react-auth0-spa";
 import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
 
 const Landing = () => {
-  const { loading, isAuthenticated, loginWithRedirect } = useAuth0();
-
-  if (loading) return <p>Loading...</p>;
   return (
     <Card
       bg="dark"
@@ -18,31 +14,19 @@ const Landing = () => {
       style={{ minHeight: "50vh" }}
     >
       <h5 className="m-4">Welcome to Pure WACC</h5>
-      {!isAuthenticated && (
-        <div>
-          <Button variant="secondary" onClick={() => loginWithRedirect({})}>
-            <FaIcon icon={faKey} style={{ marginRight: "0.4rem" }}></FaIcon>
-            Sign up
-          </Button>
-        </div>
-      )}
-      {isAuthenticated && (
-        <>
-          <Button
-            variant="outline-primary"
-            onClick={() => console.log("create new project")}
-          >
-            <FaIcon icon={faPlus} className="mr-2"></FaIcon>
-            <Link
-              to="/project"
-              className="text-light"
-              style={{ textDecoration: "none" }}
-            >
-              New Project
-            </Link>
-          </Button>
-        </>
-      )}
+      <Button
+        variant="outline-primary"
+        onClick={() => console.log("create new project")}
+      >
+        <FaIcon icon={faPlus} className="mr-2"></FaIcon>
+        <Link
+          to="/project"
+          className="text-light"
+          style={{ textDecoration: "none" }}
+        >
+          New Project
+        </Link>
+      </Button>
     </Card>
   );
 };
