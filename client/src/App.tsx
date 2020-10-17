5c6978ca2a41ee7a6f86b4158902b3edb0ecc755
Teodor Nilseng Danielsen <teodor.nilseng@gmail.com>
2020-10-17 20:53:39 +0200

    adding Calculator and AdminTool components to App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,18 +8,22 @@ import NavBar from "./components/NavBar";
 import Landing from "./components/Landing";
 import Project from "./components/Project";
 import Footer from "./components/Footer";
+import Calculator from "./components/Calculator";
+import AdminTool from "./components/AdminTool";
 
 function App() {
   return (
     <Router history={history}>
       <NavBar />
       <Container
-        className="p-4 d-flex flex-column justify-content-center"
+        className="px-0 py-5 d-flex flex-column"
         style={{ minHeight: "calc(100vh - 259px)" }}
       >
         <Switch>
           <Route path="/" exact component={Landing} />
           <Route path="/project" component={Project} />
+          <Route path="/calculator" component={Calculator} />
+          <Route path="/admin" component={AdminTool} />
         </Switch>
       </Container>
       <Footer />
