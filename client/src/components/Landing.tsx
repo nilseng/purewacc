d8151633126d397ddcf32a233c2227048b8b1457
Teodor Nilseng Danielsen <teodor.nilseng@gmail.com>
2020-10-20 20:00:08 +0200

    removing card from Landing

--- client/src/components/Landing.tsx
+++ client/src/components/Landing.tsx
@@ -2,16 +2,11 @@ import React from "react";
 import { FontAwesomeIcon as FaIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
-import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
-    <Card
-      bg="dark"
-      className="d-flex flex-column justify-content-center align-items-center p-4 my-4"
-      style={{ minHeight: "50vh" }}
-    >
+    <div className="d-flex flex-column align-items-center p-4">
       <h5 className="m-4">Welcome to Pure WACC</h5>
       <Link
         to="/project-tool"
@@ -21,7 +16,7 @@ const Landing = () => {
         <FaIcon icon={faPlus} className="mr-2"></FaIcon>
         New Project
       </Link>
-    </Card>
+    </div>
   );
 };
 
