99ed1c3ccd2d68bbd563d74976cf7cf8b42a3e5d
Teodor Nilseng Danielsen <teodor.nilseng@gmail.com>
2020-11-03 00:22:51 +0100

    adding trailing slash to issuer

--- server/src/auth/auth.ts
+++ server/src/auth/auth.ts
@@ -17,7 +17,7 @@ export const checkJwt = jwt({
         jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
     }),
     audience: process.env.AUTH0_AUDIENCE,
-    issuer: `https://${process.env.AUTH0_DOMAIN}`,
+    issuer: `https://${process.env.AUTH0_DOMAIN}/`,
     algorithms: ['RS256']
 });
 
