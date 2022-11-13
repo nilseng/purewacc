f11d059bf058f75cb1e065bc7ca6bde52895eba2
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2022-11-13 23:54:37 +0100

    build: updates

--- server/src/auth/auth.ts
+++ server/src/auth/auth.ts
@@ -1,21 +1,18 @@
 import axios, { AxiosRequestConfig } from "axios";
 import dotenv from "dotenv";
-import { expressjwt } from "express-jwt";
+import { expressjwt, GetVerificationKey } from "express-jwt";
 import jwtAuthz from "express-jwt-authz";
 import jwksRsa from "jwks-rsa";
 
 dotenv.config();
 
 export const checkJwt = expressjwt({
-  // Dynamically provide a signing key
-  // based on the kid in the header and
-  // the signing keys provided by the JWKS endpoint.
   secret: jwksRsa.expressJwtSecret({
     cache: true,
     rateLimit: true,
     jwksRequestsPerMinute: 5,
     jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
-  }),
+  }) as GetVerificationKey,
   audience: process.env.AUTH0_AUDIENCE,
   issuer: `https://${process.env.AUTH0_DOMAIN}/`,
   algorithms: ["RS256"],
